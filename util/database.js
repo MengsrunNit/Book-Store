d670143e8db4896eab4d8fb1ad115d7b12c97f82
Mengsrun Nit <nitmengsrun@gmail.com>
2025-09-09 02:06:16 -0400

    add dot enviroment and git ignore

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
 const Sequelize = require("sequelize");
+require('dotenv').config();
 
-const sequelize = new Sequelize("my_new_db", "root", "Mengsrun2@7", {
-  host: "127.0.0.1",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASS, 
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
